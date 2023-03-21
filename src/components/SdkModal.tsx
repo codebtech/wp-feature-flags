122dd359bc42a36508c649988ba275d216dae965
Mohan Raj <mohanraj.pachaiyappan@news.co.uk>
2023-03-21 20:07:32 +0000

    adds code highlighter component

--- src/components/SdkModal.tsx
+++ src/components/SdkModal.tsx
@@ -1,15 +1,22 @@
 import { Modal, Button } from '@wordpress/components';
+import Snippet from './Snippet';
 
 const SdkModal = (props: any): JSX.Element => {
 	const { item, closeSdkModal } = props;
+
+	const jsSnippet = `if (window.mrFeatureFlags.isEnabled('${item.name}')) {
+	// js code goes here...
+}`;
+
+	const phpSnippet = `if ( /\MR/\FeatureFlags/\FeatureFlags::is_enabled( '${item.name}' ) ) {
+	// php code goes here...
+}`;
 	return (
 		<Modal title={`SDK snippets`} onRequestClose={closeSdkModal}>
-			<p>{`if (window.mrFeatureFlags.isEnabled('DeskNet')) {
-	render(<SettingsPage />, document.getElementById('desknet_settings'));
-}`}</p>
-			<p>{`if ( \MR\FeatureFlags\FeatureFlags::is_enabled( 'Teal' ) ) {
-    add_theme_support('menus');
-}`}</p>
+			<h3>JS Snippet</h3>
+			<Snippet data={jsSnippet} />
+			<h3>PHP Snippet</h3>
+			<Snippet data={phpSnippet} />
 			<Button variant="tertiary" onClick={closeSdkModal}>
 				Close
 			</Button>
