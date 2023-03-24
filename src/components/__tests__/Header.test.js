0d3ec67757ca2a26402d6650bd63868c5fb5b600
Mohan Raj <mohanraj.pachaiyappan@news.co.uk>
2023-03-24 09:32:24 +0000

    add jest rtl configs

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../Header';
+
+describe('Header Component', () => {
+	it('should render without error', () => {
+		expect(1).toBe(1);
+	});
+});
