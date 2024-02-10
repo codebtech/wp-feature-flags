59c01774c63379837863aebe24a4cbb8f7a139fe
Mohan Raj <mohanraj.pachaiyappan@news.co.uk>
2024-02-10 07:16:25 +0000

    adds yoast integration test setup

--- tests/integration/HelloTest.php
+++ tests/integration/HelloTest.php
@@ -4,8 +4,11 @@
  * @since 0.1.0
  */
 
-class HelloIntegrationTest extends \WP_UnitTestCase {
+use Yoast\WPTestUtils\WPIntegration\TestCase;
 
+ class HelloIntegrationTest extends TestCase {
+
+	// TODO: https://core.trac.wordpress.org/ticket/57844
 	public function test_hello_integration_test():void {
 		$this->assertTrue( post_type_exists( 'page' ) );
 	}
