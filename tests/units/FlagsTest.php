1cc8322b525e29e10cca81914a223be133f2f26b
Mohan Raj <mohanraj.pachaiyappan@news.co.uk>
2024-02-11 16:56:28 +0000

    remove unused tests

--- tests/units/FlagsTest.php
+++ tests/units/FlagsTest.php
@@ -37,51 +37,5 @@ class FlagsTest extends TestCase
 		$result = $flags->get_all_flags();
 		$this->assertEquals($result, $mock_option_value);
 	}
-
-	public function test_post_flags_methods_should_return_success_if_input_is_array() {
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		  );
-		$request_mock = \Mockery::mock('WP_Request');
-		$request_mock->shouldReceive('get_json_params')->andReturn(['param1' => 'value1']);
-
-		\Brain\Monkey\Functions\when('update_option')->justReturn(true);
-		\Brain\Monkey\Functions\when('rest_ensure_response')->returnArg();
-
-		global $wp;
-		$wp = new \stdClass();
-		$wp->request = $request_mock;
-
-		$flags = new Flags();
-		$result = $flags->post_flags($request_mock);
-
-		$this->assertEquals(['status'=>200, 'success' => true], $result);
-
-        unset($GLOBALS['wp']);
-    }
-
-	public function test_post_flags_methods_should_throw_error_if_input_is_not_an_array() {
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		  );
-        $request_mock = \Mockery::mock('WP_Request');
-        $request_mock->shouldReceive('get_json_params')->andReturn('test');
-
-        global $wp;
-        $wp = new \stdClass();
-        $wp->request = $request_mock;
-
-		$error_mock = \Mockery::mock('WP_Error');
-
-		\Brain\Monkey\Functions\expect('post_flags')->andReturn($error_mock);
-
-
-		$flags = new Flags();
-		$result = $flags->post_flags($request_mock);
-
-		$this->assertInstanceOf('WP_Error', $result);
-
-    }
-
-
+	
 }
