d5e0bb8aeb206fd8ff7d491d8b0d0506503ac2e2
m0hanraj <m0hanraj@users.noreply.github.com>
2024-03-02 18:41:01 +0000

    Built plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeb/wp-feature-flags',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b28879e645eadd9231dafa2894a3ddb47b56eb0a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeb/wp-feature-flags' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b28879e645eadd9231dafa2894a3ddb47b56eb0a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
