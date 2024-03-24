6931ecebe68853f7934f15fe40a1cf929d84cb02
m0hanraj <m0hanraj@users.noreply.github.com>
2024-03-24 11:31:20 +0000

    Built plugin

--- src/settings.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from '@wordpress/element';
-import Flags from './components/Flags';
-import './styles/settings.scss';
-const container = document.getElementById(
-	'codeb_feature_flags_settings_screen'
-);
-const root = container && createRoot(container);
-root?.render(<Flags />);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeb/wp-feature-flags',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'aace75e44f250533be7783cf077dca5874d9ac11',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeb/wp-feature-flags' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'aace75e44f250533be7783cf077dca5874d9ac11',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
